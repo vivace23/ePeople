93ef48bc8f888b78f37d4dc356ce6f4153c51652
avydevy <avydevy@gmail.com>
2025-02-25 01:46:35 +0000

    initialisation de express pour la creation de l'api

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.SERVER_PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
