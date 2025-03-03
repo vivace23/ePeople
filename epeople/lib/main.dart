e5ffb9f0fa839babc2b9f4ace6ce37727947bb6f
vivace23 <godehiyane@gmail.com>
2025-03-03 00:40:34 +0000

    apres avoir reglé tout les problemes de l'application pour pouvoir l'executer sur le telephone

--- epeople/lib/main.dart
+++ epeople/lib/main.dart
@@ -8,14 +8,15 @@ import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async{
-  await dotenv.load(fileName: ".env");
+  //await dotenv.load(fileName: ".env");
   
-  WidgetsFlutterBinding.ensureInitialized();
-
+  //WidgetsFlutterBinding.ensureInitialized();
+  /*
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+  */
 
   runApp(const MyApp());
 }
